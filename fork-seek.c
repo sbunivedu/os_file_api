228862b9bb72041989ecabcb4510dd2ed7a258c9
Baochuan Lu <blu@sbuniv.edu>
2022-04-28 13:54:03 +0000

    add parent child share OFT entry example

---
+++ fork-seek.c
@@ -0,0 +1,21 @@
+#include <assert.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]) {
+  int fd = open("fork-seek.c", O_RDONLY);
+  assert(fd >= 0);
+  int rc = fork();
+  if (rc == 0) {
+    rc = lseek(fd, 10, SEEK_SET);
+    printf("child: offset %d\n", rc);
+  } else if (rc > 0) {
+    (void) wait(NULL);
+    printf("parent: offset %d\n", (int) lseek(fd, 0, SEEK_CUR));
+  }
+  return 0;
+}
+
